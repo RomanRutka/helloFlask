a4c8c4a8cbe7395f81f1cdf692195a614e9af0cf
RomanRutka <roman.rutka@gmail.com>
2022-06-08 14:57:52 +0200

    Committing some minor changes before other tests

--- helloflask/flask_app.py
+++ helloflask/flask_app.py
@@ -29,6 +29,7 @@ def index(user = 'Roman'):						# association d’une route (URL) avec la foncti
 	conn.close()
 	return render_template('index.html', name = user, posts=posts)
 
+
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
